0f2ec6317998a9a724e3feee3820433fb774795b
gogolescargot <gauthiergalon@pm.me>
2025-07-03 14:17:15 +0200

    Format

--- src/main.rs
+++ src/main.rs
@@ -17,193 +17,146 @@ fn parse(expression: &String) -> Result<(Vec<String>, Vec<String>), String> {
 	Ok((left, right))
 }
 
-fn coefficient(string: String) -> Result<f64, String>
-{
+fn coefficient(string: String) -> Result<f64, String> {
 	let cleaned_string = string
 		.replace("X^2", "")
 		.replace("X^1", "")
 		.replace("X^0", "")
 		.replace("*", "");
-	if cleaned_string == "-"
-	{
+	if cleaned_string == "-" {
 		return Ok(-1.);
 	}
-	if cleaned_string.is_empty()
-	{
+	if cleaned_string.is_empty() {
 		return Ok(1.);
 	}
 	cleaned_string.parse::<f64>().map_err(|e| e.to_string())
 }
 
-fn decompose(left: Vec<String>, right: Vec<String>) -> Result<[f64; 3], String>
-{
+fn decompose(left: Vec<String>, right: Vec<String>) -> Result<[f64; 3], String> {
 	let mut a: f64 = 0.;
 	let mut b: f64 = 0.;
 	let mut c: f64 = 0.;
 
-	for elem in left
-	{
-		if elem.find("X^2").is_some()
-		{
-			a += match coefficient(elem)
-			{
+	for elem in left {
+		if elem.find("X^2").is_some() {
+			a += match coefficient(elem) {
 				Ok(value) => value,
 				Err(err) => return Err(err),
 			};
-		}
-		else if elem.find("X^1").is_some()
-		{
-			b += match coefficient(elem)
-			{
+		} else if elem.find("X^1").is_some() {
+			b += match coefficient(elem) {
 				Ok(value) => value,
 				Err(err) => return Err(err),
 			};
-		}
-		else if elem.find("X^0").is_some()
-		{
-			c += match coefficient(elem)
-			{
+		} else if elem.find("X^0").is_some() {
+			c += match coefficient(elem) {
 				Ok(value) => value,
 				Err(err) => return Err(err),
 			};
-		}
-		else if elem.find("X^").is_some()
-		{
-			return Err(format!("Error: Unsupported polynomial term: '{}'", elem))
-		}
-		else if elem != "0"
-		{
-			return Err(format!("Error: Unsupported expression: '{}'", elem))
+		} else if elem.find("X^").is_some() {
+			return Err(format!("Error: Unsupported polynomial term: '{}'", elem));
+		} else if elem != "0" {
+			return Err(format!("Error: Unsupported expression: '{}'", elem));
 		}
 	}
 
 	for elem in right {
-		if elem.find("X^2").is_some()
-		{
-			a -= match coefficient(elem)
-			{
+		if elem.find("X^2").is_some() {
+			a -= match coefficient(elem) {
 				Ok(value) => value,
 				Err(err) => return Err(err),
 			};
-		}
-		else if elem.find("X^1").is_some()
-		{
-			b -= match coefficient(elem)
-			{
+		} else if elem.find("X^1").is_some() {
+			b -= match coefficient(elem) {
 				Ok(value) => value,
 				Err(err) => return Err(err),
 			};
-		}
-		else if elem.find("X^0").is_some() {
-			c -= match coefficient(elem)
-			{
+		} else if elem.find("X^0").is_some() {
+			c -= match coefficient(elem) {
 				Ok(value) => value,
 				Err(err) => return Err(err),
 			};
-		}
-		else if elem.find("X^").is_some()
-		{
-			return Err(format!("Error: Unsupported polynomial term: '{}'", elem))
-		}
-		else if elem != "0"
-		{
-			return Err(format!("Error: Unsupported expression: '{}'", elem))
+		} else if elem.find("X^").is_some() {
+			return Err(format!("Error: Unsupported polynomial term: '{}'", elem));
+		} else if elem != "0" {
+			return Err(format!("Error: Unsupported expression: '{}'", elem));
 		}
 	}
 
 	Ok([a, b, c])
 }
 
-fn reduced(&coeffs: &[f64; 3])
-{
+fn reduced(&coeffs: &[f64; 3]) {
 	print!("Reduced form: ");
 	let mut first = true;
-	for (i, coeff) in coeffs.iter().enumerate()
-	{
-		if *coeff < 0.
-		{
+	for (i, coeff) in coeffs.iter().enumerate() {
+		if *coeff < 0. {
 			print!("- ");
-		}
-		else if *coeff > 0. && !first
-		{
+		} else if *coeff > 0. && !first {
 			print!("+ ");
 		}
-		if *coeff == 1. || *coeff == -1.
-		{
-			match i
-			{
-				2 => {},
-				1 => {print!("X ");},
-				_ => {print!("X^{} ", 2 - i);}
+		if *coeff == 1. || *coeff == -1. {
+			match i {
+				2 => {}
+				1 => {
+					print!("X ");
+				}
+				_ => {
+					print!("X^{} ", 2 - i);
+				}
+			}
+			first = false;
+		} else if *coeff != 0. {
+			match i {
+				2 => {
+					print!("{} ", coeff.abs());
+				}
+				1 => {
+					print!("{} * X ", coeff.abs());
+				}
+				_ => {
+					print!("{} * X^{} ", coeff.abs(), 2 - i);
+				}
 			}
 			first = false;
 		}
-		else if *coeff != 0.
-		{
-			match i
-			{
-				2 => {print!("{} ", coeff.abs());},
-				1 => {print!("{} * X ", coeff.abs());},
-				_ => {print!("{} * X^{} ", coeff.abs(), 2 - i);}
-			}
-			first = false;
-		}
-		
 	}
-	if first
-	{
+	if first {
 		print!("0 ");
 	}
 	println!("= 0");
 }
 
-fn degree(coeffs: [f64; 3]) -> i8
-{
-	if coeffs[0] != 0.
-	{
-		return 2
+fn degree(coeffs: [f64; 3]) -> i8 {
+	if coeffs[0] != 0. {
+		return 2;
 	}
-	if coeffs[1] != 0.
-	{
-		return 1
+	if coeffs[1] != 0. {
+		return 1;
 	}
-	return 0
+	return 0;
 }
 
-fn solve(a: f64, b: f64, c: f64, degree: i8)
-{
-	if degree == 0
-	{
-		if c == 0.
-		{
+fn solve(a: f64, b: f64, c: f64, degree: i8) {
+	if degree == 0 {
+		if c == 0. {
 			println!("Any real number is a solution.");
-		}
-		else
-		{
+		} else {
 			println!("No solution.");
 		}
-	}
-	else if degree == 1
-	{
+	} else if degree == 1 {
 		println!("Solution : x = {}", -c / b);
-	}
-	else
-	{
+	} else {
 		let delta: f64 = b.powf(2.) - 4. * a * c;
 
-		if delta > 0.
-		{
+		if delta > 0. {
 			println!("Discriminant is strictly positive, the two solutions are:");
 			let x1 = (-b - delta.sqrt()) / (2. * a);
 			let x2 = (-b + delta.sqrt()) / (2. * a);
 			println!("{}\n{}", x1, x2);
-		}
-		else if delta == 0.
-		{
+		} else if delta == 0. {
 			println!("The solution is:\n{}", -b / (2. * a));
-		}
-		else
-		{
+		} else {
 			println!("Discriminant is strictly negative, the two complex solutions are:");
 			let real = -b / (2. * a);
 			let imaginary = (-delta).sqrt() / (2. * a);
@@ -212,12 +165,10 @@ fn solve(a: f64, b: f64, c: f64, degree: i8)
 	}
 }
 
-fn computor()
-{
+fn computor() {
 	let arg: Vec<String> = env::args().collect();
 
-	if arg.len() != 2
-	{
+	if arg.len() != 2 {
 		println!("Usage: computor <expression>");
 		return;
 	}
@@ -226,8 +177,7 @@ fn computor()
 
 	let parsed = parse(expression);
 
-	if parsed.is_err()
-	{
+	if parsed.is_err() {
 		println!("{}", parsed.unwrap_err());
 		return;
 	}
@@ -236,8 +186,7 @@ fn computor()
 
 	let decomposed = decompose(left, right);
 
-	if decomposed.is_err()
-	{
+	if decomposed.is_err() {
 		println!("{}", decomposed.unwrap_err());
 		return;
 	}
@@ -253,7 +202,6 @@ fn computor()
 	solve(coeffs[0], coeffs[1], coeffs[2], degree);
 }
 
-fn main()
-{
+fn main() {
 	computor();
 }
--- src/test.rs
+++ src/test.rs
@@ -4,20 +4,25 @@ use std::process::Command;
 #[cfg(test)]
 fn run_test(input: &str, expected_output: &str) {
 	let build_status = Command::new("cargo")
-        .arg("build")
-        .arg("--release")
-        .status()
-        .expect("Failed to execute `cargo build --release`");
+		.arg("build")
+		.arg("--release")
+		.status()
+		.expect("Failed to execute `cargo build --release`");
 
-    assert!(build_status.success(), "Build failed");
+	assert!(build_status.success(), "Build failed");
 
 	let output = Command::new("target/release/computorv1")
-	.arg(input)
-	.output()
-	.expect("Failed to execute process");
+		.arg(input)
+		.output()
+		.expect("Failed to execute process");
 
 	let stdout = String::from_utf8_lossy(&output.stdout);
-	assert_eq!(stdout.trim(), expected_output.trim(), "Test failed for input: {}", input);
+	assert_eq!(
+		stdout.trim(),
+		expected_output.trim(),
+		"Test failed for input: {}",
+		input
+	);
 }
 
 #[test]
@@ -32,7 +37,7 @@ fn test_case_1() {
 fn test_case_2() {
 	run_test(
 		"5 * X^0 + 4 * X^1 = 4 * X^0",
-		"Reduced form: 4 * X + = 0\nPolynomial degree: 1\nSolution : x = -0.25"
+		"Reduced form: 4 * X + = 0\nPolynomial degree: 1\nSolution : x = -0.25",
 	);
 }
 
@@ -40,7 +45,7 @@ fn test_case_2() {
 fn test_case_3() {
 	run_test(
 		"8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0",
-		"Error: Unsupported polynomial term: '-5.6*X^3'"
+		"Error: Unsupported polynomial term: '-5.6*X^3'",
 	);
 }
 
@@ -48,7 +53,7 @@ fn test_case_3() {
 fn test_case_4() {
 	run_test(
 		"6 * X^0 = 6 * X^0",
-		"Reduced form: 0 = 0\nPolynomial degree: 0\nAny real number is a solution."
+		"Reduced form: 0 = 0\nPolynomial degree: 0\nAny real number is a solution.",
 	);
 }
 
@@ -56,7 +61,7 @@ fn test_case_4() {
 fn test_case_5() {
 	run_test(
 		"10 * X^0 = 15 * X^0",
-		"Reduced form: - 5 = 0\nPolynomial degree: 0\nNo solution."
+		"Reduced form: - 5 = 0\nPolynomial degree: 0\nNo solution.",
 	);
 }
 
